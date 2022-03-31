89d679273a88f9ae2da7796a2bbdc55dc431e42d
xhcstudy <99958269+xhcstudy@users.noreply.github.com>
2022-03-31 10:03:25 +0800

    Add files via upload
    结构体3

---
+++ test57/test57-1/test57-1/test57-1.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//结构体传参   尽量选择传址
+//函数传参的时候,参数是需要压栈，会有时间和空间上的系统开销,
+//如果传递一个结构体对象的时候，结构体过大，参数压栈的的系统开销比较大，所以会导致性能的下降。
+
+struct S
+{
+	int a;
+	char c;
+	double d;
+};
+
+void Init(struct S* ps)
+{
+	ps->a = 100;
+	ps->c = 'w';
+	ps->d = 3.14;
+}
+
+//传值
+void Print1(struct S temp)
+{
+	printf("%d %c %lf\n", temp.a, temp.c, temp.d);
+}
+
+//传址
+void Print2(const struct S* ps)
+{
+	printf("%d %c %lf\n", ps->a, ps->c, ps->d);
+}
+
+int main()
+{
+	struct S s = { 0 };
+	Init(&s);
+	//Print1(s);
+	Print2(&s);
+	return 0;
+} 
